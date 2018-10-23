3537eb4b9e1251182358638675d0dbdc7992be4f
Julien Moeys <julienmoeys@yahoo.fr>
2018-10-23 10:45:50 +0200

    Make source script more generic

--- package_source.r
+++ package_source.r
@@ -1,8 +1,14 @@
 
 rm(list=ls(all=TRUE)) 
 
-pkgName <- "macroutils2"
-setwd( file.path( Sys.getenv( x = "rPackagesDir" ), pkgName ) )
+#   Project name
+prjName <- "macrounchained"
 
-#   Source macroutils2
-source( "R/macroutils2.r" )
+#   Package name (in the project)
+pkgName <- "macroutils2"
+
+setwd( file.path( Sys.getenv( x = "rPackagesDir" ), prjName, 
+    pkgName ) )
+
+#   Source rmacrolite
+source( sprintf( "R/%s.r", pkgName ) )
