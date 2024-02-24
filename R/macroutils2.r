c0873e17c88aeea305fd7e4e708a48d33ec8b6b0
Julien Moeys <julienmoeys@yahoo.fr>
2024-02-24 21:36:20 +0100

    Use "_PACKAGE" keyword for -package.R

--- R/macroutils2.r
+++ R/macroutils2.r
@@ -23,30 +23,6 @@ NULL
 
 
 
-
-#' R utility functions for the MACRO (and SOIL) models.
-#'
-#' R utility functions for the MACRO (and SOIL) models. Reads, 
-#'  writes, plot, view and converts MACRO binary files (input or 
-#'  output data).
-#'
-#'@name macroutils2-package
-#'
-#'@aliases macroutils2-package macroutils
-#'
-#'@docType package
-#'
-#'@author 
-#'  Julien MOEYS <jules_m78-soiltexture@@yahooDOTfr>, 
-#'  Kristian Persson <kristianDOTpersson@@sluDOTse> 
-#'
-#'  Maintainer: Julien MOEYS <jules_m78-soiltexture@@yahooDOTfr>, 
-#'
-#'@keywords package
-NULL
-
-
-
 # +-------------------------------------------------------------+ 
 # | Original file: onAttach.R                                   | 
 # +-------------------------------------------------------------+ 
